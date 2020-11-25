57b0800dc7a7c167398370c2569b2d04765b9928
Nathaniel Smith <ngs.new@gmail.com>
2020-11-25 01:50:53 -0500

    Created world visualizer to visualize the world model

--- include/visualizer/world_visualizer.h
+++ include/visualizer/world_visualizer.h
@@ -1,5 +1,46 @@
 //
 // Created by Nathaniel Smith on 11/16/20.
 //
-
 #pragma once
+
+#include "core/world_model.h"
+#include "visualizer_helpers.h"
+
+namespace antares {
+
+namespace visualizer {
+
+class CinderMap {
+
+
+public:
+
+    CinderMap();
+
+    CinderMap(const models::World &world);
+
+    /**
+    * Updates the state for of the container model
+    */
+    void UpdateState();
+
+    /**
+    * Renders the current state of the model
+    */
+    void Render() const;
+
+    /**
+    * Resets the state of the model
+     * Not implemented :(
+    */
+    void Reset();
+
+    /** container model for the particles */
+
+    models::World world_model_;
+
+};
+
+} //namespace visualizer
+
+} //namespace antares
--- src/visualizer/world_visualizer.cc
+++ src/visualizer/world_visualizer.cc
@@ -4,3 +4,39 @@
 
 #include "visualizer/world_visualizer.h"
 
+namespace antares {
+
+namespace visualizer {
+
+
+
+
+    visualizer::CinderMap::CinderMap() {
+
+    }
+
+    visualizer::CinderMap::CinderMap(const models::World &world) {
+
+    }
+
+    void visualizer::CinderMap::UpdateState() {
+        world_model_.UpdateState();
+    }
+
+    void visualizer::CinderMap::Render() const {
+
+        int color_differentiator = 0;
+        for (const b2Body* body = world_model_.world_.GetBodyList(); body; body = body->GetNext()) {
+            DrawPolygon(body, world_model_.kPixelsPerMeterFactor,
+                        ci::Color8u(255 - color_differentiator , color_differentiator, 150));
+            color_differentiator = 150;
+        }
+    }
+
+    void visualizer::CinderMap::Reset() {
+
+    }
+
+} //namespace visualizer
+
+} //namespace antares
