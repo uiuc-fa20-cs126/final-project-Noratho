d356da5ba27d35cf1e8e6fabbe6ce07854447ba7
Nathaniel Smith <ngs.new@gmail.com>
2020-12-07 23:04:10 -0500

    created a move handler to handle moves initiation

---
+++ include/core/move_handler.h
@@ -0,0 +1,30 @@
+//
+// Created by Nathaniel Smith on 12/7/20.
+//
+
+#pragma once
+
+#include "move_model.h"
+
+namespace antares {
+
+namespace models {
+
+    class MoveHandler {
+
+    public:
+
+    private:
+
+        Shield current_shield_;
+
+        Attack current_attack_;
+
+        MobilityMove current_movement_;
+
+
+    };
+
+} //namespace models
+
+} //namespace antares
---
+++ src/core/move_handler.cc
@@ -0,0 +1,15 @@
+//
+// Created by Nathaniel Smith on 12/7/20.
+//
+
+#include "core/move_handler.h"
+
+
+namespace antares {
+
+namespace models {
+
+
+} //namespace models
+
+} //namespace antares
