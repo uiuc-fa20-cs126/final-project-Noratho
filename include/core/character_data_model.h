20c08bf002887f2fae50fd54a61cf4d88fd54d38
Nathaniel Smith <ngs.new@gmail.com>
2020-12-08 22:44:21 -0500

    Moved all of the getter and setter implementation into the .cc file and added specs

--- include/core/character_data_model.h
+++ include/core/character_data_model.h
@@ -15,293 +15,287 @@ namespace antares {
 
 namespace models {
 
+/**
+* AirAttacks struct
+* Stores data regarding air attacks
+*/
     struct AirAttacks {
 
     public:
-
+        /**Json macro*/
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(AirAttacks, neutral_air, forward_air,
                 back_air, up_air, down_air);
 
-        const Attack &GetNeutralAir() const {
-            return neutral_air;
-        }
+        /**Getters and Setters*/
+        const Attack &GetNeutralAir() const;
 
-        const Attack &GetForwardAir() const {
-            return forward_air;
-        }
+        const Attack &GetForwardAir() const;
 
-        const Attack &GetBackAir() const {
-            return back_air;
-        }
+        const Attack &GetBackAir() const;
 
-        const Attack &GetUpAir() const {
-            return up_air;
-        }
+        const Attack &GetUpAir() const;
 
-        const Attack &GetDownAir() const {
-            return down_air;
-        }
+        const Attack &GetDownAir() const;
 
-        const std::map<std::string, Attack> &GetInputMapRight() const {
-            return input_map_right;
-        }
+        const std::map<std::string, Attack> &GetInputMapRight() const;
 
-        const std::map<std::string, Attack> &GetInputMapLeft() const {
-            return input_map_left;
-        }
+        const std::map<std::string, Attack> &GetInputMapLeft() const;
 
-        void SetInputMapRight(const std::map<std::string, Attack> &inputMapRight) {
-            input_map_right = inputMapRight;
-        }
+        void SetInputMapRight(const std::map<std::string, Attack> &inputMapRight);
 
-        void SetInputMapLeft(const std::map<std::string, Attack> &inputMapLeft) {
-            input_map_left = inputMapLeft;
-        }
-
+        void SetInputMapLeft(const std::map<std::string, Attack> &inputMapLeft);
 
     private:
 
+        /**A map of inputs to right*/
         std::map<std::string, Attack> input_map_right;
+        /**A map of inputs to left*/
         std::map<std::string, Attack> input_map_left;
 
+        /**Neutral Air Attack*/
         Attack neutral_air;
+        /**forward Air Attack*/
         Attack forward_air;
+        /**back Air Attack*/
         Attack back_air;
+        /**up Air Attack*/
         Attack up_air;
+        /**down Air Attack*/
         Attack down_air;
 
     };
 
+/**
+* Specials struct
+* Stores data regarding special moves
+*/
     struct Specials {
 
     public:
+        /**Json macro*/
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(Specials, neutral_special, down_special,
                 up_special, left_special, right_special);
 
-        const Attack &GetNeutralSpecial() const {
-            return neutral_special;
-        }
+        /**Getters and Setters*/
+        const Attack &GetNeutralSpecial() const;
 
-        const Attack &GetLeftSpecial() const {
-            return left_special;
-        }
+        const Attack &GetLeftSpecial() const;
 
-        const Attack &GetRightSpecial() const {
-            return right_special;
-        }
+        const Attack &GetRightSpecial() const;
 
-        const Attack &GetDownSpecial() const {
-            return down_special;
-        }
+        const Attack &GetDownSpecial() const;
 
-        const Attack &GetUpSpecial() const {
-            return up_special;
-        }
+        const Attack &GetUpSpecial() const;
 
-        const std::map<std::string, Attack> &GetInputMap() const {
-            return input_map;
-        }
+        const std::map<std::string, Attack> &GetInputMap() const;
 
-        void SetInputMap(const std::map<std::string, Attack> &inputMap) {
-            input_map = inputMap;
-        }
-
+        void SetInputMap(const std::map<std::string, Attack> &inputMap);
 
     private:
+        /**A map of inputs*/
         std::map<std::string, Attack> input_map;
 
+        /**neutral special Attack*/
         Attack neutral_special;
+        /**left special Attack*/
         Attack left_special;
+        /**right special Attack*/
         Attack right_special;
+        /**down special Attack*/
         Attack down_special;
+        /**up special Attack*/
         Attack up_special;
 
     };
 
+/**
+* GroundedNormals struct
+* Stores data regarding grounded normal moves
+*/
     struct GroundedNormals {
 
     public:
+        /**Json macro*/
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(GroundedNormals, jab, right_tilt, left_tilt, down_tilt, up_tilt,
                 right_strong, left_strong, down_strong, down_strong, up_strong);
 
-        const Attack &GetJab() const {
-            return jab;
-        }
+        /**Getters and Setters*/
+        const Attack &GetJab() const;
 
-        const Attack &GetRightTilt() const {
-            return right_tilt;
-        }
+        const Attack &GetRightTilt() const;
 
-        const Attack &GetLeftTilt() const {
-            return left_tilt;
-        }
+        const Attack &GetLeftTilt() const;
 
-        const Attack &GetUpTilt() const {
-            return up_tilt;
-        }
+        const Attack &GetUpTilt() const;
 
-        const Attack &GetDownTilt() const {
-            return down_tilt;
-        }
+        const Attack &GetDownTilt() const;
 
-        const Attack &GetRightStrong() const {
-            return right_strong;
-        }
+        const Attack &GetRightStrong() const;
 
-        const Attack &GetLeftStrong() const {
-            return left_strong;
-        }
+        const Attack &GetLeftStrong() const;
 
-        const Attack &GetUpStrong() const {
-            return up_strong;
-        }
+        const Attack &GetUpStrong() const;
 
-        const Attack &GetDownStrong() const {
-            return down_strong;
-        }
+        const Attack &GetDownStrong() const;
 
-        const std::map<std::string, Attack> &GetInputMapTilts() const {
-            return input_map_tilts;
-        }
+        const std::map<std::string, Attack> &GetInputMapTilts() const;
 
-        const std::map<std::string, Attack> &GetInputMapStrong() const {
-            return input_map_strong;
-        }
+        const std::map<std::string, Attack> &GetInputMapStrong() const;
 
-        void SetInputMapTilts(const std::map<std::string, Attack> &inputMapTilts) {
-            input_map_tilts = inputMapTilts;
-        }
+        void SetInputMapTilts(const std::map<std::string, Attack> &inputMapTilts);
 
-        void SetInputMapStrong(const std::map<std::string, Attack> &inputMapStrong) {
-            input_map_strong = inputMapStrong;
-        }
+        void SetInputMapStrong(const std::map<std::string, Attack> &inputMapStrong);
 
     private:
 
+        /**A map of inputs for tilts attacks*/
         std::map<std::string, Attack> input_map_tilts;
+        /**A map of inputs for strong attacks*/
         std::map<std::string, Attack> input_map_strong;
 
+        /**jab attack*/
         Attack jab;
+        /**right tilt attack*/
         Attack right_tilt;
+        /**left tilt attack*/
         Attack left_tilt;
+        /**up tilt attack*/
         Attack up_tilt;
+        /**down tilt attack*/
         Attack down_tilt;
 
+        /**right strong attack*/
         Attack right_strong;
+        /**left strong attack*/
         Attack left_strong;
+        /**up strong attack*/
         Attack up_strong;
+        /**down strong attack*/
         Attack down_strong;
 
     };
 
+/**
+* Defense struct
+* Stores data regarding defensive moves
+*/
     struct Defense {
 
     public:
+        /**Json macro*/
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(Defense, shield, roll_left, roll_right, spot_dodge,
                 air_dodge);
 
-        const Shield &GetShield() const {
-            return shield;
-        }
+        /**Getters and Setters*/
+        const Shield &GetShield() const;
 
-        const MobilityMove &GetRollLeft() const {
-            return roll_left;
-        }
+        const MobilityMove &GetRollLeft() const;
 
-        const MobilityMove &GetRollRight() const {
-            return roll_right;
-        }
+        const MobilityMove &GetRollRight() const;
 
-        const MobilityMove &GetSpotDodge() const {
-            return spot_dodge;
-        }
+        const MobilityMove &GetSpotDodge() const;
 
-        const MobilityMove &GetAirDodge() const {
-            return air_dodge;
-        }
+        const MobilityMove &GetAirDodge() const;
 
-        const std::map<std::string, MobilityMove> &GetInputMap() const {
-            return input_map;
-        }
+        const std::map<std::string, MobilityMove> &GetInputMap() const;
 
-        void SetInputMap(const std::map<std::string, MobilityMove> &inputMap) {
-            input_map = inputMap;
-        }
+        void SetInputMap(const std::map<std::string, MobilityMove> &inputMap);
 
     private:
 
+        /**An input map for defensive moves*/
         std::map<std::string, MobilityMove> input_map;
 
+        /**Shield mobility move*/
         Shield shield;
 
+        /**roll right mobility move*/
         MobilityMove roll_left;
+        /**roll left mobility move*/
         MobilityMove roll_right;
+        /**spot dodge mobility move*/
         MobilityMove spot_dodge;
+        /**air dodge mobility move*/
         MobilityMove air_dodge;
 
     };
 
-
+/**
+* MoveSet struct
+* Holds all the data for a character move set
+*/
     struct MoveSet {
 
     public:
+        /**Json macro*/
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(MoveSet, jump, defense, grounded_normals, specials, air_attacks);
 
-        Defense &GetDefense() {
-            return defense;
-        }
+        /**Getters and Setters*/
+        Defense &GetDefense();
 
-        GroundedNormals &GetGroundedNormals() {
-            return grounded_normals;
-        }
+        GroundedNormals &GetGroundedNormals();
 
-        AirAttacks &GetAirAttacks() {
-            return air_attacks;
-        }
+        AirAttacks &GetAirAttacks();
 
-        Specials &GetSpecials() {
-            return specials;
-        }
+        Specials &GetSpecials();
 
     private:
+        /**Jump Mobility Move*/
         MobilityMove jump;
+        /**Field to hold defense moves*/
         Defense defense;
+        /**Field to hold grounded normals*/
         GroundedNormals grounded_normals;
+        /**Field to hold air attacks*/
         AirAttacks air_attacks;
+        /**Field to hold specials*/
         Specials specials;
 
     };
 
+/**
+* SpriteSet struct
+* Holds all the data for a different sprites(unimplemented)
+*/
     struct SpriteSet {
 
     public:
+        /**Json macro*/
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(SpriteSet, idle, hit, out, jump, run, turn);
 
     private:
+        /**idle sprite path*/
         std::string idle;
+        /**hit sprite path*/
         std::string hit;
+        /**death sprite path*/
         std::string out;
 
+        /**jump sprite path*/
         std::string jump;
+        /**run sprite path*/
         std::string run;
+        /**turn sprite path*/
         std::string turn;
     };
 
+/**
+* CharacterData struct
+* Holds all the data for a character
+*/
     struct CharacterData {
 
     public:
-
+        /**Json macro*/
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(CharacterData, character_name, fall_speed_multiplier,
                                        fast_fall_speed_multiplier, run_speed, jump_height,
                                        move_set, sprite_set, hurt_boxes_data);
 
+        /**Constructor's hurt box*/
         CharacterData();
 
+        /**Getters and Setters*/
         MoveSet& GetMoveSet();
-
-        float GetFallSpeedMultiplier() const;
-
-        float GetFastFallSpeedMultiplier() const;
 
         float GetRunSpeed() const;
 
@@ -311,25 +305,29 @@ namespace models {
 
     private:
 
-        //float health;
-
+        /**character's name*/
         std::string character_name;
 
+        /**the rate a character falls(unimplemented)*/
         float fall_speed_multiplier;
-
+        /**The speed a character goes down when fast falling(unimplemented)*/
         float fast_fall_speed_multiplier;
+
+        /**multiplier for character run speed*/
         float run_speed;
+        /**height a character can jump*/
         float jump_height;
 
+        /**character's move set*/
         MoveSet move_set;
 
+        /**character's sprite set*/
         SpriteSet sprite_set;
 
+        /**character's hurt box*/
         std::vector<b2CircleShapeDataHolder> hurt_boxes_data;
 
-
     };
-
 
 } //namespace models
 
--- src/core/character_data_model.cc
+++ src/core/character_data_model.cc
@@ -15,14 +15,6 @@ const std::vector<b2CircleShapeDataHolder> &CharacterData::GetHurtBoxesData() co
     return hurt_boxes_data;
 }
 
-float CharacterData::GetFallSpeedMultiplier() const {
-    return fall_speed_multiplier;
-}
-
-float CharacterData::GetFastFallSpeedMultiplier() const {
-    return fast_fall_speed_multiplier;
-}
-
 float CharacterData::GetRunSpeed() const {
     return run_speed;
 }
@@ -33,6 +25,166 @@ float CharacterData::GetJumpHeight() const {
 
 MoveSet &CharacterData::GetMoveSet() {
     return this->move_set;
+}
+
+Defense &MoveSet::GetDefense() {
+    return defense;
+}
+
+GroundedNormals &MoveSet::GetGroundedNormals() {
+    return grounded_normals;
+}
+
+AirAttacks &MoveSet::GetAirAttacks() {
+    return air_attacks;
+}
+
+Specials &MoveSet::GetSpecials() {
+    return specials;
+}
+
+const Shield &Defense::GetShield() const {
+    return shield;
+}
+
+const MobilityMove &Defense::GetRollLeft() const {
+    return roll_left;
+}
+
+const MobilityMove &Defense::GetRollRight() const {
+    return roll_right;
+}
+
+const MobilityMove &Defense::GetSpotDodge() const {
+    return spot_dodge;
+}
+
+const MobilityMove &Defense::GetAirDodge() const {
+    return air_dodge;
+}
+
+const std::map<std::string, MobilityMove> &Defense::GetInputMap() const {
+    return input_map;
+}
+
+void Defense::SetInputMap(const std::map<std::string, MobilityMove> &inputMap) {
+    input_map = inputMap;
+}
+
+const Attack &GroundedNormals::GetJab() const {
+    return jab;
+}
+
+const Attack &GroundedNormals::GetRightTilt() const {
+    return right_tilt;
+}
+
+const Attack &GroundedNormals::GetLeftTilt() const {
+    return left_tilt;
+}
+
+const Attack &GroundedNormals::GetUpTilt() const {
+    return up_tilt;
+}
+
+const Attack &GroundedNormals::GetDownTilt() const {
+    return down_tilt;
+}
+
+const Attack &GroundedNormals::GetRightStrong() const {
+    return right_strong;
+}
+
+const Attack &GroundedNormals::GetLeftStrong() const {
+    return left_strong;
+}
+
+const Attack &GroundedNormals::GetUpStrong() const {
+    return up_strong;
+}
+
+const Attack &GroundedNormals::GroundedNormals::GetDownStrong() const {
+    return down_strong;
+}
+
+const std::map<std::string, Attack> &GroundedNormals::GetInputMapTilts() const {
+    return input_map_tilts;
+}
+
+const std::map<std::string, Attack> &GroundedNormals::GetInputMapStrong() const {
+    return input_map_strong;
+}
+
+void GroundedNormals::SetInputMapTilts(const std::map<std::string, Attack> &inputMapTilts) {
+    input_map_tilts = inputMapTilts;
+}
+
+void GroundedNormals::SetInputMapStrong(const std::map<std::string, Attack> &inputMapStrong) {
+    input_map_strong = inputMapStrong;
+}
+
+const Attack &Specials::GetNeutralSpecial() const {
+    return neutral_special;
+}
+
+const Attack &Specials::GetLeftSpecial() const {
+    return left_special;
+}
+
+const Attack &Specials::GetRightSpecial() const {
+    return right_special;
+}
+
+const Attack &Specials::GetDownSpecial() const {
+    return down_special;
+}
+
+const Attack &Specials::GetUpSpecial() const {
+    return up_special;
+}
+
+const std::map<std::string, Attack> &Specials::GetInputMap() const {
+    return input_map;
+}
+
+void Specials::SetInputMap(const std::map<std::string, Attack> &inputMap) {
+    input_map = inputMap;
+}
+
+const Attack &AirAttacks::GetNeutralAir() const {
+    return neutral_air;
+}
+
+const Attack &AirAttacks::GetForwardAir() const {
+    return forward_air;
+}
+
+const Attack &AirAttacks::GetBackAir() const {
+    return back_air;
+}
+
+const Attack &AirAttacks::GetUpAir() const {
+    return up_air;
+}
+
+const Attack &AirAttacks::GetDownAir() const {
+    return down_air;
+}
+
+const std::map<std::string, Attack> &AirAttacks::GetInputMapRight() const {
+    return input_map_right;
+}
+
+const std::map<std::string, Attack> &AirAttacks::GetInputMapLeft() const {
+    return input_map_left;
+}
+
+void AirAttacks::SetInputMapRight(const std::map<std::string, Attack> &inputMapRight) {
+    input_map_right = inputMapRight;
+}
+
+void AirAttacks::SetInputMapLeft(const std::map<std::string, Attack> &inputMapLeft) {
+    input_map_left = inputMapLeft;
 }
 
 } //namespace models
