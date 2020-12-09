8fe42d5586ebfac64b3c1ceddbb0f98e0bd625d9
Nathaniel Smith <ngs.new@gmail.com>
2020-12-08 22:49:24 -0500

    Added specs and comments

--- include/core/map_model.h
+++ include/core/map_model.h
@@ -11,29 +11,34 @@ namespace antares {
 
 namespace models {
 
-
+/**
+* Map Class
+* Stores data for the map
+*/
     class Map {
 
     public:
 
+        /**Default constructor*/
         Map();
 
-        Map(std::string json_path);
-
-        std::string map_json_path_;
-
-        void DeserializeJson();
-
+        /**
+         * Method that generates the map and attaches it to the world as a fixed body
+         * @param world the world to fix the map bodies to
+         * @param pixel_per_meter_factor the factor to downscale the physics value for better running
+         * @param window_size the size of the window
+        */
         void GenerateMap(b2World &world, float pixel_per_meter_factor, std::vector<float> window_size);
 
+        /**Getters and setters*/
         float GetKGroundHeight() const;
 
     private:
 
+        /**Height of the ground in pixels*/
         const float kGroundHeight = 10.0f;
 
     };
-
 
 } //namespace models
 
--- src/core/map_model.cc
+++ src/core/map_model.cc
@@ -1,7 +1,6 @@
 //
 // Created by Nathaniel Smith on 12/6/20.
 //
-#include <iostream>
 #include "core/map_model.h"
 
 
@@ -13,32 +12,19 @@ Map::Map() {
 
 }
 
-Map::Map(std::string json_path) {
-
-}
-
-void Map::DeserializeJson() {
-
-}
-
 void Map::GenerateMap(b2World &world, float pixel_per_meter_factor, std::vector<float> window_size) {
 
     float meter_per_pixel_factor = (1.0f / pixel_per_meter_factor);
 
     float window_length = window_size[0];
     float window_height = window_size[1];
-
-    //multiply values shown by pixels per meter factor to shrink down
-
-    //    b2Vec2 throw_test = b2Vec2(1, 1);
-    //
-    //    body->SetLinearVelocity(throw_test);
+    //multiply values shown by pixels per meter factor to shrink down computation load
 
     //Bottom platform
-
     b2BodyDef ground_body_def;
     ground_body_def.position.Set(window_length / 2.0f * meter_per_pixel_factor,
-                                     (window_height - kGroundHeight) * meter_per_pixel_factor);
+                                 (window_height - kGroundHeight) * meter_per_pixel_factor);
+
     b2PolygonShape ground_box;
     ground_box.SetAsBox((window_length/2) * meter_per_pixel_factor, kGroundHeight * meter_per_pixel_factor);
     b2Body* ground_body = world.CreateBody(&ground_body_def);
